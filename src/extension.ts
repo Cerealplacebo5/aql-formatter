4ce30bdb1e9ac9510ad87ae01cb46d6012ccb298
Dieudonné Cédric <47348193+Cerealplacebo5@users.noreply.github.com>
2024-12-14 18:43:06 +0100

    [FIX] Commented thebad example and renamed the good example

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,25 @@
 'use strict';
-
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
 
     // 👎 formatter implemented as separate command
-    vscode.commands.registerCommand('extension.format-foo', () => {
-        const {activeTextEditor} = vscode.window;
+    // vscode.commands.registerCommand('extension.format-foo', () => {
+    //     const {activeTextEditor} = vscode.window;
 
-        if (activeTextEditor && activeTextEditor.document.languageId === 'foo-lang') {
-            const {document} = activeTextEditor;
-            const firstLine = document.lineAt(0);
-            if (firstLine.text !== '42') {
-                const edit = new vscode.WorkspaceEdit();
-                edit.insert(document.uri, firstLine.range.start, '42\n');
-                return vscode.workspace.applyEdit(edit)
-            }
-        }
-    });
+    //     if (activeTextEditor && activeTextEditor.document.languageId === 'foo-lang') {
+    //         const {document} = activeTextEditor;
+    //         const firstLine = document.lineAt(0);
+    //         if (firstLine.text !== '42') {
+    //             const edit = new vscode.WorkspaceEdit();
+    //             edit.insert(document.uri, firstLine.range.start, '42\n');
+    //             return vscode.workspace.applyEdit(edit)
+    //         }
+    //     }
+    // });
 
     // 👍 formatter implemented using API
-    vscode.languages.registerDocumentFormattingEditProvider('foo-lang', {
+    vscode.languages.registerDocumentFormattingEditProvider('aql-formatter', {
         provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
             const firstLine = document.lineAt(0);
             if (firstLine.text !== '42') {
